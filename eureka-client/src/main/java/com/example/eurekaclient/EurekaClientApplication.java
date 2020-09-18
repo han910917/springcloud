661497054f8cd4f61188d68f5f983deefdae457a
han910917 <han910917@163.com>
2020-09-18 17:03:45 +0800

    删除多余引用

--- eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
@@ -1,13 +1,9 @@
 package com.example.eurekaclient;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.stereotype.Component;
-
-import java.util.Map;
 
 @SpringBootApplication
 @EnableEurekaClient
