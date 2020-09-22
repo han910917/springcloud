ac9a08fe719206a230fbe53a218a89ed67e171d6
han910917 <han910917@163.com>
2020-09-22 19:57:47 +0800

    config模块

---
+++ config-client/src/main/java/com/example/configclient/controller/testController.java
@@ -0,0 +1,22 @@
+package com.example.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author hgm
+ * @Time 2020/9/22 19:24
+ */
+@RestController
+public class testController {
+
+    @Value("person.user")
+    private String user;
+
+    @RequestMapping("/configClient")
+    public Object configClient(){
+        return "hi " + user;
+    }
+}
